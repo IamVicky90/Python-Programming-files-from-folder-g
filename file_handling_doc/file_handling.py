7a67218a481e6d64ef191836014228716e3dec49
Waqas Bilal <waqasbilal02@gmail.com>
2020-04-21 18:40:38 +0500

    Adding Json and csv files in file handling program

--- file_handling_doc/file_handling.py
+++ file_handling_doc/file_handling.py
@@ -34,3 +34,25 @@ with open('test.html','r') as fr:
                     if i=='"':
                         i='\n'
                     fa.write(i)
+#######################CSV FILES#######################################
+import csv
+with open('file.csv') as f:
+    r=csv.reader(f)
+    for content in r:
+        print(content)
+        
+###### write csv file####
+
+with open('file.csv','a',newline="") as f:
+    write_content=csv.writer(f)
+    write_content.writerow(['waqas bilal 1','2020','Pakistan'])
+
+########################JSON FILES#############################
+import json
+with open('file.json','w') as f:
+    json.dump({'name':'waqas','age':'20'},f)
+with open('file.json','r') as f:
+    contents=json.load(f)
+    for key,values in contents.items():
+        print(key,values)
+
